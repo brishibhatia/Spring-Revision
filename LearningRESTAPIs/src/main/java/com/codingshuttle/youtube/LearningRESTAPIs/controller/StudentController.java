5382f735c97feea7e3ac2f3eebdf982e5388e5b3
Rishi Bhatia <brishi11128@gmail.com>
2025-11-20 22:15:55 +0530

    first commit

--- LearningRESTAPIs/src/main/java/com/codingshuttle/youtube/LearningRESTAPIs/controller/StudentController.java
+++ LearningRESTAPIs/src/main/java/com/codingshuttle/youtube/LearningRESTAPIs/controller/StudentController.java
@@ -1,14 +1,24 @@
 package com.codingshuttle.youtube.LearningRESTAPIs.controller;
 
 import com.codingshuttle.youtube.LearningRESTAPIs.dto.StudentDto;
+import com.codingshuttle.youtube.LearningRESTAPIs.entity.Student;
+import com.codingshuttle.youtube.LearningRESTAPIs.repository.StudentRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class StudentController {
-@GetMapping("/student")
-    public StudentDto getStudent(){
-        return new StudentDto(2L , "Rishi" , "brihi@gmail.com");
+    private final StudentRepository studentRepository;
+
+    public StudentController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @GetMapping("/student")
+    public List<Student> getStudent(){
+        return studentRepository.findAll();
     }
     @GetMapping("/student/{id}")
     public StudentDto getStudentbyid(){
