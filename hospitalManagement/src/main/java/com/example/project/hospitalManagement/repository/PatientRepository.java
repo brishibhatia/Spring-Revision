d9b78eaa258e14cda90bc385f578ff0c3170d8df
Rishi Bhatia <brishi11128@gmail.com>
2025-12-04 20:45:25 +0530

    yes

---
+++ hospitalManagement/src/main/java/com/example/project/hospitalManagement/repository/PatientRepository.java
@@ -0,0 +1,10 @@
+package com.example.project.hospitalManagement.repository;
+
+import com.example.project.hospitalManagement.Entity.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient , Long>{
+
+}
---
+++ hospitalManagement/src/test/java/com/example/project/hospitalManagement/PatientTests.java
@@ -0,0 +1,21 @@
+package com.example.project.hospitalManagement;
+
+import com.example.project.hospitalManagement.Entity.Patient;
+import com.example.project.hospitalManagement.repository.PatientRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class PatientTests {
+    @Autowired
+    private PatientRepository patientRepository;
+
+   @Test
+    public void testPatientRepository(){
+        List<Patient> PatientList =  patientRepository.findAll();
+        System.out.println(PatientList);
+    }
+}
